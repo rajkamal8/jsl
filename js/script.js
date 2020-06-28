361b0a354107c5fc6977326e74ca0c18605aaee3
rajkamal8 <rajkamal.gupta.it23@heritageit.edu.in>
2020-06-29 01:06:53 +0530

    #

--- js/script.js
+++ js/script.js
@@ -1,39 +1,57 @@
-/*var x="hello world";
-console.log(x);
-var my="my name is raj";
-console.log(my);*/
-var message= "in global";
-console.log("global:message = "+message);
+// /*var x="hello world";
+// console.log(x);
+// var my="my name is raj";
+// console.log(my);*/
+// var message= "in global";
+// console.log("global:message = "+message);
 
-var a = function()
-{
-    var message = "inside a";
-    console.log("a : message = "+message);
-    b();
-}
-a();
+// var a = function()
+// {
+//     var message = "inside a";
+//     console.log("a : message = "+message);
+//     b();
+// }
+// a();
 
-function b ()
-{
-    console.log("b :message = "+message);
-}
-/* ***************************************************/
-var a1 = function()
-{
-    var message = "inside a1";
-    console.log("a1 : message = "+ message);
+// function b ()
+// {
+//     console.log("b :message = "+message);
+// }
+// /* ***************************************************/
+// var a1 = function()
+// {
+//     var message = "inside a1";
+//     console.log("a1 : message = "+ message);
 
-    function b1 ()
-    {
-        console.log("b1 :message = "+ message);
-    }
-    b1();
-}
-a1();
+//     function b1 ()
+//     {
+//         console.log("b1 :message = "+ message);
+//     }
+//     b1();
+// }
+// a1();
 
-/* ***************************************************/
-message="hi";
-console.log(message);
+// /* ***************************************************/
+// message="hi";
+// console.log(message);
+
+
+// function factory and passing function 
+// var person={
+//     name:"raj",
+//     phone:123,
+//     last:"kamal"
+// };
+// console.log(person["name"]);
+
+// var people=new Object();
+// people.name="rajat";
+// people.detail=new Object();
+// people.detail.phones=12345;
+// people.detail.catname="sicily";
+// console.log(people);
+
+
 
 
 
